aca9cca5cf24fa90c2212aa197723d5748fd018a
Melis Nur KILIÇ <fiorebucaneve22@gmail.com>
2021-06-05 23:10:22 +0300

    api commit 1

---
+++ http_json_api_apps/android/app/src/main/java/melisnur/http_json_api_apps/MainActivity.java
@@ -0,0 +1,6 @@
+package melisnur.http_json_api_apps;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ http_json_api_apps/lib/local_json.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LocalJsonKullanimi extends StatefulWidget {
+  @override
+  _LocalJsonKullanimiState createState() => _LocalJsonKullanimiState();
+}
+
+class _LocalJsonKullanimiState extends State<LocalJsonKullanimi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
